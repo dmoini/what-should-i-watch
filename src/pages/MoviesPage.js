dab7815d4e72fdbadeee6e32e165bfdf4af1cb79
Donovan Moini <donovanmoini@gmail.com>
2020-04-16 22:30:56 -0700

    Integrated TMDB movie mock data for SearchResultLIst component

--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -2,21 +2,15 @@ import { Button, Grid, Typography } from "@material-ui/core";
 import React, { useState } from "react";
 
 import AverageRatingFilter from "../components/AverageRatingFilter";
-import Background from "../images/moviesBackground.jpg";
 import CountryFilter from "../components/CountryFilter";
 import GenreFilter from "../components/GenreFilter";
+import SearchResultList from "../components/SearchResultList";
+import TMDB_MOVIES_MOCK_DATA from "../mock/mockMoviesData";
 import YearRangeFilter from "../components/YearRangeFilter";
 import { makeStyles } from "@material-ui/core/styles";
 import { moviesTheme } from "../common/categoryThemes";
 
 const useStyles = makeStyles({
-  background: {
-    // backgroundImage: `url(${Background})`,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    height: "100vh",
-  },
   button: {
     backgroundColor: moviesTheme.backgroundColor,
     "&:hover": {
@@ -27,8 +21,9 @@ const useStyles = makeStyles({
     fontWeight: "bold",
   },
   grid: {
-    minHeight: "100vh",
-    paddingBottom: "500px",
+    minHeight: "50vh",
+    paddingTop: "80px",
+    paddingBottom: "80px",
   },
   title: {
     color: moviesTheme.backgroundColor,
@@ -42,6 +37,7 @@ export default function MoviesPages() {
   const [startYear, setStartYear] = useState("");
   const [endYear, setEndYear] = useState("");
   const [averageRating, setAverageRating] = useState("");
+  const [clickedSearch, setClickedSearch] = useState(false);
   const handleYearChange = {
     startYear: (e) => setStartYear(e.target.value),
     endYear: (e) => setEndYear(e.target.value),
@@ -51,7 +47,12 @@ export default function MoviesPages() {
   const isNumber = (s) => /^\d+$/.test(s);
   const isValidRating = (s) => /^(10|(\d(\.\d+)?))$/.test(s);
 
-  const checkUserInput = () => {
+  const invalidUserInput = () => {
+    if ([genre, country, startYear, endYear, averageRating].every((v) => !v)) {
+      window.alert("Please use at least one filter.");
+      return true;
+    }
+
     if (
       (startYear && !isNumber(startYear)) ||
       (endYear && !isNumber(endYear))
@@ -59,70 +60,82 @@ export default function MoviesPages() {
       window.alert(
         "If using Start Year and/or End Year filters, make sure they are valid years."
       );
-      return;
+      return true;
     }
 
     if (averageRating && !isValidRating(averageRating)) {
       window.alert(
         "If using Average Rating filter, make sure it is a valid decimal between 1-10."
       );
-      return;
+      return true;
     }
 
-    if ([genre, country, startYear, endYear, averageRating].every((v) => !v)) {
-      window.alert("Please use at least one filter.");
+    return false;
+  };
+
+  const handleSearch = () => {
+    if (invalidUserInput()) {
       return;
     }
+    console.log(
+      `Genre: ${genre}\nCountry: ${country}\nStart Year: ${startYear}\nEnd Year: ${endYear}\nAverage Rating:${averageRating}`
+    );
+
+    setClickedSearch(true);
   };
 
   return (
-    <Grid
-      className={classes.grid}
-      container
-      spacing={1}
-      direction="column"
-      alignItems="center"
-      justify="center"
-    >
-      <Grid item>
-        <Typography variant="h2" className={classes.title}>
-          Find Movies
-        </Typography>
+    <div>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={1}
+        direction="column"
+        alignItems="center"
+        justify="center"
+      >
+        <Grid item>
+          <Typography variant="h2" className={classes.title}>
+            Find Movies
+          </Typography>
+        </Grid>
+        <Grid item>
+          <GenreFilter
+            currentGenre={genre}
+            handleChange={(e) => setGenre(e.target.value)}
+          />
+        </Grid>
+        <Grid item>
+          <CountryFilter
+            currentCountry={country}
+            handleChange={(e) => setCountry(e.target.value)}
+          />
+        </Grid>
+        <Grid item>
+          <YearRangeFilter handleChange={handleYearChange} />
+        </Grid>
+        <Grid item>
+          <AverageRatingFilter
+            handleChange={(e) => setAverageRating(e.target.value)}
+          />
+        </Grid>
+        <Grid item>
+          <Button
+            classes={{ root: classes.button }}
+            variant="contained"
+            onClick={() => {
+              handleSearch();
+            }}
+          >
+            Search
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item>
-        <GenreFilter
-          currentGenre={genre}
-          handleChange={(e) => setGenre(e.target.value)}
-        />
-      </Grid>
-      <Grid item>
-        <CountryFilter
-          currentCountry={country}
-          handleChange={(e) => setCountry(e.target.value)}
-        />
-      </Grid>
-      <Grid item>
-        <YearRangeFilter handleChange={handleYearChange} />
-      </Grid>
-      <Grid item>
-        <AverageRatingFilter
-          handleChange={(e) => setAverageRating(e.target.value)}
-        />
-      </Grid>
-      <Grid item>
-        <Button
-          classes={{ root: classes.button }}
-          variant="contained"
-          onClick={() => {
-            checkUserInput();
-            console.log(
-              `Genre: ${genre}\nCountry: ${country}\nStart Year: ${startYear}\nEnd Year: ${endYear}\nAverage Rating:${averageRating}`
-            );
-          }}
-        >
-          Search
-        </Button>
-      </Grid>
-    </Grid>
+      <div style={{ paddingBottom: "100px" }}>
+        <>
+          {clickedSearch && <SearchResultList data={TMDB_MOVIES_MOCK_DATA} />}
+        </>
+      </div>
+    </div>
   );
 }
