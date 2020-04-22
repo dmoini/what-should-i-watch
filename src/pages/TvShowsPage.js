58f579c25a0bcceb94ec3eaa16f11ab3e4d43f9b
racheljn <rnguye12@lion.lmu.edu>
2020-04-22 02:38:55 -0500

    Made discoverTv API call

--- src/pages/TvShowsPage.js
+++ src/pages/TvShowsPage.js
@@ -1,22 +1,20 @@
 import React, { useState } from "react";
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { tvShowsTheme } from "../common/categoryThemes";
-
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
-import AverageRatingFilter from "../components/AverageRatingFilter";
+import { discoverTv } from "../api/tvShowsApi";
+import SearchResultList from "../components/SearchResultList";
 import GenreFilter from "../components/GenreFilter";
-
 
 export default function TvShowsPage() {
     const theme = {
-        spacing: (factor) => `${0.25 * factor}rem`, // (Bootstrap strategy)
+        spacing: (factor) => `${0.5 * factor}rem`
       };
 
     const useStyles = makeStyles({
@@ -28,30 +26,41 @@ export default function TvShowsPage() {
             margin: theme.spacing(1),
             minWidth: 120,
         },
-        selectEmpty: {
-            marginTop: theme.spacing(2),
-        },
+        button: {
+            backgroundColor: tvShowsTheme.backgroundColor,
+            color: tvShowsTheme.textColor
+          }
       });
 
     const classes = useStyles();
     const [genre, setGenre] = useState("");
     const [rating, setRating] = useState("");
+    const [search, setSearch] = useState(false);
+    const [tvData, setTvData] = useState({});
 
     const handleChange = (event) => {
         setRating(event.target.value);
-      };
+    };
+
+    const handleSearch = async () => {
+        const res = await discoverTv({
+            genre,
+            rating        
+        });
+
+        if (res) {
+            setTvData(res);
+            setSearch(true);
+        }
+    };
 
     return (
-        <div className={classes.background} align = "center">
-            <Typography variant = 'h5' className={classes.title}> TV Shows </Typography>
+        <div align = "center">
+            <Typography variant = 'h5' className={classes.title}>TV Shows</Typography>
             <GenreFilter onSubmit = {(e) => setGenre(e.target.value)}/>
-            <FormControl variant="filled" className={classes.formControl}>
+            <FormControl variant = "filled" className={classes.formControl}>
                 <InputLabel shrink id = "simple-select-placeholder-label-label">Rating</InputLabel>
-                <Select
-                    value={rating}
-                    onChange={handleChange}
-                    className={classes.selectEmpty}
-                >
+                <Select value={rating} onChange = { handleChange } >
                     <MenuItem value={1}>1</MenuItem>
                     <MenuItem value={2}>2</MenuItem>
                     <MenuItem value={3}>3</MenuItem>
@@ -64,6 +73,14 @@ export default function TvShowsPage() {
                     <MenuItem value={10}>10</MenuItem>
                 </Select>
             </FormControl>
+            <div>
+                <Button classes = {{ root: classes.button }} onClick = { async () => { await handleSearch(); }}>
+                    Search
+                </Button>
+                <div style={{ paddingBottom: "100px" }}>
+                    <>{ search && <SearchResultList data = { tvData } />}</>
+                </div>
+            </div>
         </div>
     );
 }
